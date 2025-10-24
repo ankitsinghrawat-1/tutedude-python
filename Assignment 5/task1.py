96fc9137ee207b708a5a09132fc7202dd0835f4c
ankitsinghrawat-1 <ankitrawat54328@gmail.com>
2025-10-24 23:11:55 +0530

    updation

--- Assignment 5/task1.py
+++ Assignment 5/task1.py
@@ -1,4 +1,4 @@
-marks = {"ankit" : 100, "vrishti": 90, "rahul" : 85, "sonam" : 80, "sachin" : 70, "anurag" : 60}
+marks = {"ankit" : 100, "ryan": 90, "rahul" : 85, "sonam" : 80, "sachin" : 70, "anurag" : 60}
 
 name = input("Enter the name of the student: ")
 if name.lower() in marks:
